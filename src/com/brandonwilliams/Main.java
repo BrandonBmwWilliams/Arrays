566068d20267d0e97d2213fb982eb85a0e31f9f2
Brandon Williams <brandonbmwwilliams@gmail.com>
2020-03-08 12:38:51 -0700

    Project showing how to utilize arrays for averaging numbers from user input.

---
+++ src/com/brandonwilliams/Main.java
@@ -0,0 +1,28 @@
+package com.brandonwilliams;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[]   myIntegers = getIntegers(5);
+    }
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer valued. \r" );
+        int[]values = new int [number];
+
+        for (int i=0; i<values. length; i++){
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i =0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / (double)array.length;
+    }
+}
